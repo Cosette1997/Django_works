a5240db5c5a5e863daff094c078db70b11a6bfa8
Cosette1997 <1489847871@qq.com>
2017-10-04 19:16:18 -0700

    add django project mblog

---
+++ mainsite/views.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Post
+from datetime import datetime
+from django.template.loader import get_template
+
+# Create your views here.
+
+def homepage(request):
+    posts = Post.objects.all()
+    template = get_template('index.html')
+    now = datetime.now()
+    html = template.render(locals())
+    print(type(html))
+    return HttpResponse(html)
+
+def showpost(request, slug):
+    template = get_template('post.html')
+    try:
+        post = Post.objects.get(slug=slug)
+        if post != None:
+            html = template.render(locals())
+            return HttpResponse(html)
+    except:
+        return redirect('/')
